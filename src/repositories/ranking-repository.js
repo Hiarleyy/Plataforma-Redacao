c6d00eb894207b0c7525f920f23c23bcf3433e84
Ryan Costa <ryancosta0200119@gmail.com>
2025-05-02 16:18:58 -0300

    modificação no ranking-repository

--- src/repositories/ranking-repository.js
+++ src/repositories/ranking-repository.js
@@ -6,12 +6,14 @@ const rankingRepository = {
       SELECT 
         u.id,
         u.nome,
+        t.nome AS turma,
         ROUND(AVG(c.nota), 2) AS media
       FROM "Usuario" u
+      JOIN "Turma" t ON u."turmaId" = t.id
       JOIN "Redacao" r ON u.id = r."usuarioId"
       JOIN "Correcao" c ON r.id = c."redacaoId"
       WHERE r.status = 'CORRIGIDA'
-      GROUP BY u.id, u.nome
+      GROUP BY u.id, u.nome, t.nome
       ORDER BY media DESC;
     `;
 
