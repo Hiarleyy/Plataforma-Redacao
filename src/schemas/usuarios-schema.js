89aa4ef0c8db2491a229e2b4b69d8691890df4da
Marcos Hiarley <marcoshiarley.silva@gmail.com>
2025-07-02 10:32:40 -0300

    fix: add email validation to criarUsuarioSchema and atualizarUsuarioSchema

--- src/schemas/usuarios-schema.js
+++ src/schemas/usuarios-schema.js
@@ -2,7 +2,7 @@ const { z } = require("zod")
 
 const criarUsuarioSchema = z.object({
   nome: z.string(),
-  email:z.string(),
+  email:z.string().email(),
   password: z.string().optional(),
   tipoUsuario: z.enum(["ADMIN", "STANDARD"]).default("STANDARD"),
   turmaId: z.string().uuid()
@@ -10,7 +10,7 @@ const criarUsuarioSchema = z.object({
 
 const atualizarUsuarioSchema = z.object({
   nome: z.string().optional(),
-  email: z.string().optional(),
+  email: z.string().email().optional(),
   password: z.string().optional(),
   caminho: z.string().optional(),
   tipoUsuario: z.enum(["ADMIN", "STANDARD"]).optional(),
